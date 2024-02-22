5d0e05ef377c95d024c703dac1a52bb0b39ab84c
AndileCartist <andilemkhizekhabazela@gmail.com>
2024-02-22 09:24:38 +0200

    pushed database

---
+++ db/dbManipulation/Views/rejectedStudentsView.sql
@@ -0,0 +1,17 @@
+USE BBD_BursaryDB;
+
+GO
+
+CREATE VIEW RejectedStudents
+AS
+  SELECT S.[StudentID], U.[FirstName], U.[LastName], B.[BursaryApplicantStatus], C.[Email], C.[PhoneNumber]
+  FROM [Student] AS S
+    INNER JOIN [User_Details] AS U
+    ON S.[UserID] = U.[UserID]
+    INNER JOIN [Contact_Details] AS C
+     ON U.[ContactDetailsID] = C.[ContactDetailsID]
+    INNER JOIN [Bursary_Applicants] AS B
+    ON S.[StudentID] = B.[StudentID]
+    INNER JOIN [Application_Status] AS A
+    ON B.[BursaryApplicantStatus] = A.[Index]
+  WHERE A.[Status] = 'rejected';
