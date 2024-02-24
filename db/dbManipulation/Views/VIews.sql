0facdaf6c57a625780a1d8e0657782537793b360
Philani Ngcamu <Philaningcamu18@gmail.com>
2024-02-24 17:04:13 +0200

    Sample data

--- db/dbManipulation/Views/VIews.sql
+++ db/dbManipulation/Views/VIews.sql
@@ -1,13 +1,13 @@
 
 
-CREATE VIEW allstudentApplications AS
 
-    SELECT S.ID_Number, U.[FirstName], U.[LastName], B.[BursaryAmount], B.[Motivation], A.[Status]
-    FROM [Student] AS S
-        INNER JOIN [User_Details] AS U
-        ON S.[UserID] = U.[UserID]
-        INNER JOIN [Bursary_Applicants] AS B
-        ON S.[StudentID] = B.[StudentID]
-        INNER JOIN [Application_Status] AS A
-        ON B.[BursaryApplicationStatusID] = A.[ApplicationStatusID];
-
+CREATE VIEW [dbo].[Student_Bursary_Application]
+AS
+ SELECT U.[FirstName], U.[LastName], dbo.getStudentuniversity (B.HeadofdepartmentId) as University,B.AverageMarks, B.[BursaryAmount], B.[Motivation], A.[Status]
+  FROM [Student] AS S
+    INNER JOIN [User_Details] AS U
+    ON S.[UserID] = U.[UserID]
+    INNER JOIN [Bursary_Applicants] AS B
+    ON S.[StudentID] = B.[StudentID]
+    INNER JOIN [Application_Status] AS A
+    ON B.[BursaryApplicationStatusID] = A.[ApplicationStatusID];
